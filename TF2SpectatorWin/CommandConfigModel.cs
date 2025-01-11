9c23bf34727fa856b05a2602fa9132d1f0adaaf3
rotatcepS <id.rotatcepS@gmail.com>
2025-01-10 21:38:34 -0700

    clean up default commands

--- TF2SpectatorWin/CommandConfigModel.cs
+++ TF2SpectatorWin/CommandConfigModel.cs
@@ -138,43 +138,35 @@ namespace TF2SpectatorWin
 
         //read from a file, use this as backup
         private static readonly string commandConfig =
-            //toggle x // is same as incrementvar x 0 1 1 // is same as toggle x 0 1 // toggle can get a list of values to toggle through as additional args.
-
             "!voteMapA\tnext_map_vote 0\tEnd-of-Round vote for the 1st map option (next_map_vote 0)\t\r\n" +
             "!voteMapB\tnext_map_vote 1\tEnd-of-Round vote for the 2nd map option (next_map_vote 1)\t\r\n" +
             "!voteMapC\tnext_map_vote 2\tEnd-of-Round vote for the 3rd map option (next_map_vote 2)\t\r\n" +
-            "!vote|vote next map...\tnext_map_vote {1|a:0|b:1|c:2|first:0|second:1|third:2|current:0|stay:0|zero:0|one:1|two:2}\tEnd-of-Round vote for map choice 0, 1, or 2 (A, B, or C; first, second, or third)\t\r\n" +
+            "!vote|!voteMap|Vote next map...\tnext_map_vote {1|a:0|b:1|c:2|first:0|second:1|third:2|current:0|stay:0|zero:0|one:1|two:2}\tEnd-of-Round vote for map choice 0, 1, or 2 (A, B, or C; first, second, or third)\t\r\n" +
 
             "!hitSound\ttf_dingalingaling_effect\tWhat hit sound is in use?\tCurrent hit sound is {1|0:0 (Default)|1:1 (Electro)|2:2 (Notes)|3:3 (Percussion)|4:4 (Retro)|5:5 (Space)|6:6 (Beepo)|7:7 (Vortex)|8:8 (Squasher)}\r\n" +
+            "!killSound\ttf_dingalingaling_last_effect\tWhat kill sound is in use?\tCurrent kill sound is {1|0:0 (Default)|1:1 (Electro)|2:2 (Notes)|3:3 (Percussion)|4:4 (Retro)|5:5 (Space)|6:6 (Beepo)|7:7 (Vortex)|8:8 (Squasher)}\r\n" +
 
             "VR mode\tcl_first_person_uses_world_model 1;tf_taunt_first_person 1;wait 20000;cl_first_person_uses_world_model 0;tf_taunt_first_person 0\tturns on VR mode for a few minutes\t\r\n" +
-            //"tf2 die\tkill;wait 1000;kill;wait 1000;kill\tinstant death in game\t\r\n" +
-            "tf2 explode\texplode;wait 1000;explode;wait 1000;explode\texplosive instant death in game\t\r\n" +
-            //requires script setup "attempt a Taunt Kill\ttaunt_kill\tattempt to do a killing taunt if the right weapon is equipped.\t\r\n" +
+            "TF2 die\tkill;wait 700;kill;wait 700;kill\tinstant death in game\t\r\n" +
+            "TF2 explode\texplode;wait 700;explode;wait 700;explode\texplosive instant death in game\t\r\n" +
             "Big Guns\ttoggle tf_use_min_viewmodels;wait 20000;toggle tf_use_min_viewmodels\ttoggles \"min viewmodels\" for a few minutes\tGuns are {tf_use_min_viewmodels|1:small|0:big} for a few minutes\r\n" +
-            "HIDERATE!\tcl_showfps 0;wait 20000;cl_showfps 1\tturns off the game fps display for a few minutes\t\r\n" +
-            "boring HUD\ttoggle cl_hud_playerclass_use_playermodel;wait 20000;toggle cl_hud_playerclass_use_playermodel\ttoggles the 3d playermodel for a few minutes... kinda boring when off...like jpuck always has it\t3d player class model is {cl_hud_playerclass_use_playermodel|1:on|0:off} for a few minutes\r\n" +
+            "Boring HUD\ttoggle cl_hud_playerclass_use_playermodel;wait 20000;toggle cl_hud_playerclass_use_playermodel\ttoggles the 3d playermodel for a few minutes... kinda boring when off...like jpuck always has it\t3d player class model is {cl_hud_playerclass_use_playermodel|1:on|0:off} for a few minutes\r\n" +
 
             "Black & White\tmat_color_projection 4;wait 20000;mat_color_projection 0\tChanges the game to Black & White for a few minutes\t\r\n" +
             "Pixelated\tmat_viewportscale 0.1;wait 20000;mat_viewportscale 1\tChanges the game to giant pixels for a few minutes\t\r\n" +
             "Dream Mode\tmat_bloom_scalefactor_scalar 50;wait 20000;mat_bloom_scalefactor_scalar 1\tGives a lighting bloom effect like we're playing in a glowy dream\t\r\n" +
             "No Guns\ttoggle r_drawviewmodel;wait 20000;toggle r_drawviewmodel\ttoggles \"draw viewmodel\" for a few minutes\tGuns are {r_drawviewmodel|1:on|0:off} for a few minutes\r\n" +
             "Long Arms\tviewmodel_fov 160;wait 20000;viewmodel_fov 54\tmaxes out \"viewmodel FOV\" for a few minutes\t\r\n" +
-            //" extreme looking:\r\n cl_pitchup/down 200 (def 89) to back/forward flip with mouse\r\n  (less than 89 to keep me from looking up/down)\r\n" +
-            //"mat_viewportscale 0.001563; mat_viewportupscale 0\r\n" +
 
             "Inspect Item\t+inspect;wait 60;-inspect\tinspect other player items or my held weapon\t\r\n" +
-            //requires script setup "SEASONAL!noisemaker|TF2 Noisemaker\tactionLoopToggle\tturn on/off my noisemaker spam if the season/loadout/server allows\t\r\n" +
-            "tf2 party chat...\tsay_party {0} in twitch says: '{1}'\te.g. \"!sayParty Hi\" says Hi to my party in tf2\t\r\n" +
+            "TF2 party chat...\tsay_party {0} in twitch says: '{1}'\te.g. \"!sayParty Hi\" says Hi to my party in tf2\t\r\n" +
 
-            "crosshair aim reset\tvoicemenu 0 1;cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200\tgives me normal crosshair settings.\t\r\n" +
-            "yes!|!yes\tvoicemenu 0 6;cl_crosshair_file crosshair3;cl_crosshair_blue 0;cl_crosshair_green 255;cl_crosshair_red 0;cl_crosshair_scale 3000;wait 500;cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200\tvisually notify me of a positive indication\t\r\n" +
-            "no!|!no\tvoicemenu 0 7;cl_crosshair_file crosshair4;cl_crosshair_blue 0;cl_crosshair_green 0;cl_crosshair_red 255;cl_crosshair_scale 2000;wait 500;cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200\tvisually notify me of a negative indication\t\r\n" +
-            "scope in|!scopeIn\tvoicemenu 2 6;cl_crosshair_file crosshair3;cl_crosshair_blue 0;cl_crosshair_green 0;cl_crosshair_red 0;cl_crosshair_scale 3000;wait 20000;cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200\tmake every class stare down the sniper scope for a few minutes.\t\r\n" +
-            "cataracts\tvoicemenu 2 5;cl_crosshair_file crosshair5;cl_crosshair_blue 255;cl_crosshair_green 255;cl_crosshair_red 255;cl_crosshair_scale 3000;wait 10000; cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200;\tsimulation of turning 44\t\r\n" +
-            "macular degeneration\tvoicemenu 2 5;cl_crosshair_file crosshair5;cl_crosshair_blue 0;cl_crosshair_green 0;cl_crosshair_red 0;cl_crosshair_scale 100;cl_crosshairalpha 200;wait 1000;cl_crosshair_scale 200;wait 1000;cl_crosshair_scale 400;wait 1000;cl_crosshair_scale 800;wait 1000;cl_crosshair_scale 1600;wait 1000;cl_crosshair_scale 3200; wait 5000; cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200;\tsimulation of macular degeneration over time\tWhile there is no cure for macular degeneration, quitting smoking, or never starting, is an important way to prevent AMD.\r\n" +
+            "Crosshair aim reset\tvoicemenu 0 1;cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200\tgives me normal crosshair settings.\t\r\n" +
+            "Scope in|!scopeIn\tvoicemenu 2 6;cl_crosshair_file crosshair3;cl_crosshair_blue 0;cl_crosshair_green 0;cl_crosshair_red 0;cl_crosshair_scale 3000;wait 20000;cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200\tmake every class stare down the sniper scope for a few minutes.\t\r\n" +
+            "Cataracts\tvoicemenu 2 5;cl_crosshair_file crosshair5;cl_crosshair_blue 255;cl_crosshair_green 255;cl_crosshair_red 255;cl_crosshair_scale 3000;wait 10000; cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200;\tsimulation of turning 44\t\r\n" +
+            "Macular degeneration\tvoicemenu 2 5;cl_crosshair_file crosshair5;cl_crosshair_blue 0;cl_crosshair_green 0;cl_crosshair_red 0;cl_crosshair_scale 100;cl_crosshairalpha 200;wait 1000;cl_crosshair_scale 200;wait 1000;cl_crosshair_scale 400;wait 1000;cl_crosshair_scale 800;wait 1000;cl_crosshair_scale 1600;wait 1000;cl_crosshair_scale 3200; wait 5000; cl_crosshair_file \"\";cl_crosshair_blue 200;cl_crosshair_green 200;cl_crosshair_red 200;cl_crosshair_scale 32;cl_crosshairalpha 200;\tsimulation of macular degeneration over time\tWhile there is no cure for macular degeneration, quitting smoking, or never starting, is an important way to prevent AMD.\r\n" +
 
-            "crosshair aim...\tcl_crosshair_file {1|1:crosshair1|2:crosshair2|3:crosshair3|4:crosshair4|5:crosshair5|6:crosshair6|7:crosshair7|reset:|normal:|(.):default|():default|Stock:default|PlusDot:crosshair1|T:crosshair2|TeeDot:crosshair2|o:crosshair3|Circle:crosshair3|x:crosshair4|Ex:crosshair4|.:crosshair5|Dot:crosshair5|PlusOpen:crosshair6|+:crosshair7|Plus:crosshair7}\tneeds one argument (like crosshair1, crosshair2 ...) - changes the crosshair to that file\t\r\n" +
+            "Crosshair aim...\tcl_crosshair_file {1|1:crosshair1|2:crosshair2|3:crosshair3|4:crosshair4|5:crosshair5|6:crosshair6|7:crosshair7|reset:|normal:|(.):default|():default|Stock:default|PlusDot:crosshair1|T:crosshair2|TeeDot:crosshair2|o:crosshair3|Circle:crosshair3|x:crosshair4|Ex:crosshair4|.:crosshair5|Dot:crosshair5|PlusOpen:crosshair6|+:crosshair7|Plus:crosshair7}\tneeds one argument (like crosshair1, crosshair2 ...) - changes the crosshair to that file\t\r\n" +
             "!aimPlusDot\tcl_crosshair_file crosshair1\t-!-\t\r\n" +
             "!aimTeeDot\tcl_crosshair_file crosshair2\t-,-\t\r\n" +
             "!aimCircle\tcl_crosshair_file crosshair3\to - a little like the projectile open circle crosshair\t\r\n" +
@@ -185,12 +177,10 @@ namespace TF2SpectatorWin
             "!aimStock\tcl_crosshair_file default\t(.) - like the weapon-spread crosshair\t\r\n" +
             "!aimBrrr\tcl_crosshair_file notafile\t!%%!\t\r\n" +
 
-            "crosshair aim size...\tcl_crosshair_scale {1|default:32|normal:32|giant:3000|big:100}\tneeds number argument - changes crosshair size from default scale size of 32\t\r\n" +
-            "crosshair aim red...\tcl_crosshair_red {1}\tset the crosshair red\taim color is now using {cl_crosshair_red} Red, {cl_crosshair_green} Green, and {cl_crosshair_blue} Blue\r\n" +
-            "crosshair aim green...\tcl_crosshair_green {1}\tset the crosshair green\taim color is now using {cl_crosshair_red} Red, {cl_crosshair_green} Green, and {cl_crosshair_blue} Blue\r\n" +
-            "crosshair aim blue...\tcl_crosshair_blue {1}\tset the crosshair blue\taim color is now using {cl_crosshair_red} Red, {cl_crosshair_green} Green, and {cl_crosshair_blue} Blue\r\n" +
-            //requires script setup "Aim Rainbow\talias renderLogo rainbowLogo;logoToggle;wait 20000;logoToggle\trainbow of crosshair colors for a few minutes.\t\r\n" +
-            "!aimSlate\tcl_crosshair_red 47;cl_crosshair_green 79;cl_crosshair_blue 79\ta color similar to slate 47 79 79\taim color is now using {cl_crosshair_red} Red, {cl_crosshair_green} Green, and {cl_crosshair_blue} Blue\r\n" +
+            "Crosshair aim size...\tcl_crosshair_scale {1|default:32|normal:32|giant:3000|big:100}\tneeds number argument - changes crosshair size from default scale size of 32\t\r\n" +
+            "Crosshair aim red...\tcl_crosshair_red {1}\tset the crosshair red\taim color is now using {cl_crosshair_red} Red, {cl_crosshair_green} Green, and {cl_crosshair_blue} Blue\r\n" +
+            "Crosshair aim green...\tcl_crosshair_green {1}\tset the crosshair green\taim color is now using {cl_crosshair_red} Red, {cl_crosshair_green} Green, and {cl_crosshair_blue} Blue\r\n" +
+            "Crosshair aim blue...\tcl_crosshair_blue {1}\tset the crosshair blue\taim color is now using {cl_crosshair_red} Red, {cl_crosshair_green} Green, and {cl_crosshair_blue} Blue\r\n" +
 
             "hit Default\ttf_dingalingaling_effect \"0\"\t\t\r\n" +
             "hit Electro\ttf_dingalingaling_effect \"1\"\t\t\r\n" +
@@ -202,23 +192,20 @@ namespace TF2SpectatorWin
             "hit Vortex\ttf_dingalingaling_effect \"7\"\t\t\r\n" +
             "hit Squasher\ttf_dingalingaling_effect \"8\"\t\t\r\n" +
             "kill Default\ttf_dingalingaling_last_effect \"0\"\t\t\r\n" +
+            "kill Electro\ttf_dingalingaling_last_effect \"1\"\t\t\r\n" +
+            "kill Notes\ttf_dingalingaling_last_effect \"2\"\t\t\r\n" +
+            "kill Percussion\ttf_dingalingaling_last_effect \"3\"\t\t\r\n" +
+            "kill Retro\ttf_dingalingaling_last_effect \"4\"\t\t\r\n" +
+            "kill Space\ttf_dingalingaling_last_effect \"5\"\t\t\r\n" +
+            "kill Beepo\ttf_dingalingaling_last_effect \"6\"\t\t\r\n" +
+            "kill Vortex\ttf_dingalingaling_last_effect \"7\"\t\t\r\n" +
+            "kill Squasher\ttf_dingalingaling_last_effect \"8\"\t\t\r\n" +
 
-            //"WHAT DEFAULT\tothertesting\tviewmodel_fov {1};wait 20000;viewmodel_fov 85\t\r\n" +
-            //"HUH 75-90\tfov_desired {1};wait 20000;fov_desired 90\t\t\r\n" +
-            //"useless?!stopWeather\ttf_particles_disable_weather 0;wait 20000;tf_particles_disable_weather 1\tturns off weather effects for a few minutes? didn't work on carrier's snow.\t\r\n" +
-            //"doesntwork!aimOpacity\tcl_crosshairalpha {1}\tneeds a number from 0-255 - changes how opaque the crosshair is from default 200\t\r\n" +
-            "pointless!showPosition\ttoggle cl_showpos;wait 20000;toggle cl_showpos\ttoggles game position info for a few minutes\t\r\n" +
-            "mehdefaultclass\tcl_class\tcurrent default class\tCurrent default class is {1}\r\n" +
-            //"DoesntWork!users\tusers\tlist users on server\t{1}\r\n" +
-            "tooAnnoyingToUse!tauntByName\ttaunt_by_name {1}\tIf it's equipped (and you got the name right), then it happens! (\"!tauntByName Taunt: The Schadenfreude\")\t\r\n" +
-            "test\tcl_allowdownload\ttest of output as arg 1\t{0} result: {1}\r\n" +
-            "test2\techo one;echo two;wait 200;echo three\ttest 2 shows result \"two\"\t{0} result: {1}\r\n" +
             "!aimColor\tcrosshair aim color...\t(just an alias)\t\r\n" +
-            //requires script setup "!aimRainbow\tAim Rainbow\t\t\r\n" +
             "!aimSize\tcrosshair aim size...\t(just an alias)\t\r\n" +
             "!resetAim\tcrosshair aim reset\t(just an alias)\t\r\n" +
-            "!bot\tkick a bot...\t(alias)" +
-            "!bots\tlist bots\t(alias)" +
+            "enableIfYouUseBotKicker!bot\tkick a bot...\t(alias)" +
+            "enableIfYouUseBotKicker!bots\tlist bots\t(alias)" +
             "!quack\tplay ambient/bumper_car_quack{random|1|2|3|4|5|9|11}.wav\tplays a duck journal sound effect\t\r\n";
 
         #region TF2Bots
